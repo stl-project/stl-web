6c949c6773f70d1d2fd34e2db4af015562277fcc
Gerrit Riessen <gorenje@urban-photogrammetry.org>
2023-01-20 16:20:51 +0100

    mobile freely countries only

--- f/site.js
+++ f/site.js
@@ -1,7 +1,7 @@
 var origFill = "#ececec";
 var highFill = "red";
 
-var allCountries = ["AE","AF","AI","AL","AM","AmericanSamoa","Angola","AntiguaandBarbuda","Argentina","AT","Australia","AW","Azerbaijan","BA","Bahamas","BB","BD","BE","BF","BG","BH","BI","BJ","BL","BM","BN","BO","BQBO","BQSA","BQSE","BR","BT","BW","BY","BZ","Canada","CanaryIslandsSpain","CapeVerde","CaymanIslands","CD","CF","CG","CH","Chile","China","CI","CM","CO","Comoros","CR","CU","CW","Cyprus","CZ","DE","Denmark","DJ","DM","DO","DZ","EC","EE","EG","EH","ER","ES","ET","FaeroeIslands","FalklandIslands","FederatedStatesofMicronesia","FI","Fiji","France","FrenchPolynesia","GA","GD","GE","GF","GH","GL","GM","GN","GQ","Greece","GT","GU","Guadeloupe","GW","GY","HN","HR","HT","HU","IE","IL","IN","Indonesia","IQ","IR","IS","Italy","Japan","JM","JO","KE","KG","KH","KP","KR","KW","KZ","LA","LB","LC","LK","LR","LS","LT","LU","LV","LY","MA","Malaysia","Malta","Mauritius","MD","ME","MF","MG","MH","MK","ML","MM","MN","MQ","MR","MS","MV","MW","MX","MZ","NA","NE","NewCaledonia","NewZealand","NG","NI","NL","NorthernMarianaIslands","Norway","NP","NR","Oman","PA","PapuaNewGuinea","PE","Philippines","PK","PL","PS","PT","PuertoRico","PW","PY","QA","RE","RO","RS","RussianFederation","RW","SA","SaintKittsandNevis","Samoa","SãoToméandPrincipe","SD","SE","Seychelles","SI","SK","SL","SN","SO","SolomonIslands","SR","SS","SV","SX","SY","SZ","TD","TG","TH","TJ","TL","TM","TN","Tonga","TrinidadandTobago","Turkey","TurksandCaicosIslands","TV","TW","TZ","UA","UG","UnitedKingdom","UnitedStates","UnitedStatesVirginIslands","UY","UZ","Vanuatu","VC","VE","VG","VN","XK","YE","YT","ZA","ZM","ZW"];
+var allCountries = ["AE","AF","AI","AL","AmericanSamoa","Angola","Argentina","AT","Australia","AW","Azerbaijan","BA","BB","BE","BF","BG","BI","BJ","BL","BM","BN","BO","BR","BW","BY","BZ","Canada","CD","CF","CG","Chile","China","CI","CM","CO","CR","CW","CZ","DE","Denmark","DJ","DZ","EC","EE","EG","EH","ER","ES","ET","FI","France","GA","GD","GE","GF","GH","GL","GM","GN","GQ","Greece","GT","GW","GY","HN","HR","HU","IN","Indonesia","IQ","IR","IS","Italy","Japan","JO","KE","KG","KH","KP","KR","KZ","LA","LK","LR","LS","LT","LV","LY","MA","Malaysia","MD","ME","MF","MG","MH","MK","ML","MM","MN","MR","MS","MV","MW","MX","MZ","NA","NE","NewZealand","NG","NI","NL","Norway","NP","NR","Oman","PA","PapuaNewGuinea","PE","Philippines","PK","PL","PT","PY","RO","RS","RussianFederation","RW","SA","Samoa","SD","SE","SI","SK","SL","SN","SO","SR","SS","SV","SX","SY","SZ","TD","TG","TH","TJ","TL","TM","TN","Tonga","Turkey","TV","TZ","UA","UG","UnitedKingdom","UnitedStates","UY","UZ","VC","VE","VG","VN","XK","YE","ZA","ZM","ZW"];
 
 var hiCountry = undefined;
 
@@ -29,29 +29,31 @@ function doIt(elem) {
   $(elem).addClass('disabled');
   $(elem).removeAttr('href');
 
-  $('#map-overlay').fadeIn(300);
-
   $('#world-map svg').find('path').css({'fill': origFill });
 
-  doCountryHighlight(30, () => {
-    setTimeout( function() {
-      var tCnt = 0;
-      var intVal = setInterval( () => {
-        if ( tCnt == 0 ){
-          tCnt = 1;
-          $('#world-map svg').find('.'+hiCountry).css({'fill': origFill});
-        } else {
-          tCnt = 0;
-          $('#world-map svg').find('.'+hiCountry).css({'fill': highFill});
-        }
-      }, 150);
+  $('#map-overlay').fadeIn(300, () => {
+    doCountryHighlight(30, () => {
+      setTimeout( function() {
+        var tCnt = 0;
+        var intVal = setInterval( () => {
+          if ( tCnt == 0 ){
+            tCnt = 1;
+            $('#world-map svg').find('.'+hiCountry).css({'fill': origFill});
+          } else {
+            tCnt = 0;
+            $('#world-map svg').find('.'+hiCountry).css({'fill': highFill});
+          }
+        }, 150);
 
-      setTimeout( () => {
-        clearInterval(intVal);
-        $('#map-overlay').fadeOut(500, () => {
-          $(elem).html("The Love has been Spread!");
-        });
-      }, 1600);
-    }, 300);
+        setTimeout( () => {
+          clearInterval(intVal);
+          $('#map-overlay').fadeOut(500, () => {
+            setTimeout( () => {
+              $(elem).html("The Love has been Spread!");
+            }, 200);
+          });
+        }, 1600);
+      }, 300);
+    })
   })
 }
