80abbf7f291890589add69052f371b34c9e6287a
Gerrit Riessen <gorenje@urban-photogrammetry.org>
2023-01-20 16:39:32 +0100

    only mobile conform countries

--- f/site.js
+++ f/site.js
@@ -1,9 +1,9 @@
 var origFill = "#ececec";
 var highFill = "red";
 
-var allCountries = ["AE","AF","AI","AL","AmericanSamoa","Angola","Argentina","AT","Australia","AW","Azerbaijan","BA","BB","BE","BF","BG","BI","BJ","BL","BM","BN","BO","BR","BW","BY","BZ","Canada","CD","CF","CG","Chile","China","CI","CM","CO","CR","CW","CZ","DE","Denmark","DJ","DZ","EC","EE","EG","EH","ER","ES","ET","FI","France","GA","GD","GE","GF","GH","GL","GM","GN","GQ","Greece","GT","GW","GY","HN","HR","HU","IN","Indonesia","IQ","IR","IS","Italy","Japan","JO","KE","KG","KH","KP","KR","KZ","LA","LK","LR","LS","LT","LV","LY","MA","Malaysia","MD","ME","MF","MG","MH","MK","ML","MM","MN","MR","MS","MV","MW","MX","MZ","NA","NE","NewZealand","NG","NI","NL","Norway","NP","NR","Oman","PA","PapuaNewGuinea","PE","Philippines","PK","PL","PT","PY","RO","RS","RussianFederation","RW","SA","Samoa","SD","SE","SI","SK","SL","SN","SO","SR","SS","SV","SX","SY","SZ","TD","TG","TH","TJ","TL","TM","TN","Tonga","Turkey","TV","TZ","UA","UG","UnitedKingdom","UnitedStates","UY","UZ","VC","VE","VG","VN","XK","YE","ZA","ZM","ZW"];
+var allCountries = ["AE","AF","AI","AL","Angola","Argentina","AT","Australia","Azerbaijan","BA","BE","BF","BG","BL","BM","BN","BO","BR","BW","BY","Canada","CD","CF","CG","Chile","China","CI","CM","CO","CZ","DE","Denmark","DZ","EC","EE","EG","ER","ES","ET","FI","France","GA","GE","GH","GL","GN","Greece","HR","HU","IN","Indonesia","IQ","IR","IS","Italy","Japan","JO","KE","KG","KH","KP","KR","KZ","LA","LK","LR","LS","LT","LV","LY","MA","Malaysia","MD","ME","MF","MG","MH","MK","ML","MM","MN","MR","MS","MV","MW","MX","MZ","NA","NE","NewZealand","NG","NL","Norway","NP","NR","Oman","PapuaNewGuinea","PE","Philippines","PK","PL","PT","PY","RO","RS","RussianFederation","SA","Samoa","SD","SE","SI","SK","SN","SO","SS","SX","SY","SZ","TD","TH","TJ","TL","TM","TN","Tonga","Turkey","TV","TZ","UA","UG","UnitedKingdom","UnitedStates","UY","UZ","VE","VG","VN","XK","YE","ZA","ZM","ZW"];
 
-var hiCountry = undefined;
+var hiCountry = undefined
 
 function highlightCountry() {
   if ( hiCountry != undefined ) {
